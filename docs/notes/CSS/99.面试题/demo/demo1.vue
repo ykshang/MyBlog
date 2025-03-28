9d5e840a660ca7b798973908b84f5ef2558afac2
kangkang shang <535634238@qq.com>
2025-03-28 22:44:24 +0800

    1

--- docs/notes/CSS/99.面试题/demo/demo1.vue
+++ docs/notes/CSS/99.面试题/demo/demo1.vue
@@ -9,21 +9,40 @@
 </template>
 
 <style scoped>
-/* 
+.container {
+  color: white;
+  display: grid;
+  grid-template-columns: 1fr 1fr; /* 两列等宽 */
+  grid-template-rows: auto auto; /* 两行自适应 */
+  grid-template-areas:
+    "top-left top-right"
+    "bottom-1 bottom-2"
+    "bottom-1 bottom-3"; /* 下1跨两行 */
+  gap: 10px; /* 网格间隙 */
+}
+
 .top-left {
-  background: burlywood;
+  height: 40px;
   grid-area: top-left;
-} */
-/* .top-right {
+  background: slategray;
+}
+.top-right {
+  height: 40px;
+  background: slategray;
   grid-area: top-right;
 }
 .bottom-1 {
+  background: slategray;
   grid-area: bottom-1;
 }
 .bottom-2 {
+  background: slategray;
+  height: 40px;
   grid-area: bottom-2;
 }
 .bottom-3 {
+  background: slategray;
+  height: 40px;
   grid-area: bottom-3;
-} */
+}
 </style>
