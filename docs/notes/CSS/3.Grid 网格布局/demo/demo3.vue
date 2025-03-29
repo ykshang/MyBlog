3c0ed9b9223eb988358a81bfe3cfc1c426d81ce0
kangkang shang <535634238@qq.com>
2025-03-29 23:15:13 +0800

    1

---
+++ docs/notes/CSS/3.Grid 网格布局/demo/demo3.vue
@@ -0,0 +1,43 @@
+<template>
+  <span class="wrapper">
+    <div class="item1">A</div>
+    <div class="item2">B</div>
+    <div class="item3">C</div>
+    <div class="item4">D</div>
+  </span>
+</template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 10px;
+  background: #f0f0f0;
+  grid-auto-rows: 60px;
+  grid-template-areas:
+    "a a a a b b b b"
+    "a a a a b b b b"
+    "c c c c d d d d"
+    "c c c c d d d d";
+}
+div {
+  background: lightblue;
+  font-size: 30px;
+  line-height: 1.2;
+}
+.item1 {
+  grid-area: a;
+}
+.item2 {
+  grid-area: b;
+  align-self: start;
+}
+.item3 {
+  grid-area: c;
+  align-self: end;
+}
+.item4 {
+  grid-area: d;
+  align-self: center;
+}
+</style>
---
+++ docs/notes/CSS/3.Grid 网格布局/demo/demo4.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <div class="item1">Item 1</div>
+    <div class="item2">Item 2</div>
+    <div class="item3">Item 3</div>
+    <div class="item4">Item 4</div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  gap: 10px;
+  grid-auto-rows: 40px;
+  background: #f0f0f0;
+  grid-template-areas:
+    "a a a a b b b b"
+    "a a a a b b b b"
+    "c c c c d d d d"
+    "c c c c d d d d";
+}
+div {
+  background: lightblue;
+  font-size: 30px;
+  line-height: 1.2;
+  padding: 10px;
+}
+.item1 {
+  grid-area: a;
+}
+.item2 {
+  grid-area: b;
+  justify-self: start;
+}
+.item3 {
+  grid-area: c;
+  justify-self: end;
+}
+.item4 {
+  grid-area: d;
+  justify-self: center;
+}
+</style>
