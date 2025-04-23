7a0fe401c91ab0d182d8203b151e394ba18db89a
kangkang shang <535634238@qq.com>
2025-04-24 00:01:31 +0800

    1

--- docs/notes/Stylus/demo/demo1.vue
+++ docs/notes/Stylus/demo/demo1.vue
@@ -3,50 +3,62 @@
     <h1 class="title">Stylus 练习页面</h1>
     <p class="content">这是一个用于练习 Stylus 语法的段落。</p>
     <button class="btn">点击我</button>
-    <br/>
-    <br/>
-    <br/>
-    <span>测试</span>
+    <br />
+    <br />
+    <span class="span1">测试 1</span>
+    <br />
+    <br />
+    <span class="span2">测试 2</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'StylusPracticePage'
-}
+  name: "StylusPracticePage",
+};
 </script>
 
 <style lang="stylus" scoped>
 // 定义变量
 $primary-color = #42b983
 $font-size-base = 16px
+color-minxin() 
+  color $primary-color
+  text-decoration underline
+  font-size 20px
+size-fun()
+  30px
 
 .container
   padding 20px
   text-align center
 
-.title
-  color $primary-color
-  font-size ($font-size-base * 2)
+  > .title
+    color $primary-color
+    font-size ($font-size-base * 2)
 
-.content
-  color #333
-  font-size $font-size-base
-  margin-top 10px
+  > .content
+    color #333
+    font-size $font-size-base
+    margin-top 10px
 
-.btn
-  background-color $primary-color
-  color white
-  border none
-  padding 10px 20px
-  border-radius 5px
-  cursor pointer
-  &:hover
-    opacity 0.8
+  > .btn
+    background-color $primary-color
+    color white
+    border none
+    padding 10px 20px
+    border-radius 5px
+    cursor pointer
 
-color-minxin()
-  color $primary-color
-div
-  span
+    &:hover
+      opacity 0.6
+
+
+
+  $primary-color = red
+  > .span1 
     color-minxin()
+  > .span2
+    color $primary-color
+    font-size size-fun()
 </style>
