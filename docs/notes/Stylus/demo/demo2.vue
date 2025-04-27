4c3d1fdb6bd5151b039324ea39704fe5bffb8b9b
kangkang shang <535634238@qq.com>
2025-04-27 11:38:24 +0800

    1

--- docs/notes/Stylus/demo/demo2.vue
+++ docs/notes/Stylus/demo/demo2.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="title">Stylus 练习页面</h1>
-    <p class="content">这是一个用于练习 Stylus 语法的段落。</p>
-    <button class="btn">点击我</button>
-    <br />
-    <br />
-    <span class="span1">
-      <span>测试 1</span>
-    </span>
-    <br />
-    <br />
-    <span class="span2">测试 2</span>
-    <br />
-    <span class="span3">测试 3</span>
-    <br />
-    <span class="span4">测试 4</span>
+    <h1 class="title">Stylus 导入导出</h1>
   </div>
 </template>
 
@@ -25,66 +11,9 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-// 定义变量
-$primary-color = #42b983
-$font-size-base = 16px
-$margin-val = 30px
-border-radius1() 
-  $block-2 = 1px
-  color $primary-color
-  text-decoration underline
-  margin 20px
-
-size-fun()
-  30px
-content-fun()
-  content
-
-margin-fun(x=10px ,y = x,z = x)
-  $block-1 = 1px;
-  (x y z)
+@require "./style/demo.styl"
 
 .container
-  padding 20px
-  text-align center
-
-  > .title
-    color $primary-color
-    font-size ($font-size-base * 2)
-
-  > .content
-    color #333
-    font-size $font-size-base
-    margin-top 10px
-
-  > .btn
-    background-color $primary-color
-    color white
-    border none
-    padding 10px 20px
-    border-radius 5px
-    cursor pointer
-    &:hover
-      opacity 0.6
-
-  $test-text = margin 16px;
-  $primary-color = red
-  $content = 'content'
-  > .span1 
-    border-radius1
-    span
-      color red
-  > .span2
-    color $primary-color
-    background: $val-color
-    font-size size-fun()
-  > .span3
-    // margin: $margin-val
-    -webkit-{text}-stroke: 1px black
-    {content-fun()} 2
-  > .span4
-    color red
-    font-size 30px
-    margin $block-2
-    padding margin-fun(10px,20px,30px)
+  color: $global-color
+  margin: margin-fun()
 </style>
