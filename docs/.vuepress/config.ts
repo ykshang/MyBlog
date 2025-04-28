864b5e6bf4ac3426a101a5adb12e1e6685fe80d1
kangkang shang <535634238@qq.com>
2025-04-28 19:28:43 +0800

    1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -100,7 +100,7 @@ export default defineUserConfig({
     //   abbr: true,         // 启用 abbr 语法  *[label]: content
     //   annotation: true,   // 启用 annotation 语法  [+label]: content
     //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
-    //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
+      caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
     //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
     //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
     //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
