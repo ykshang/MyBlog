8589a7ae21413fd29cd573a0bc2e268cc869cd38
kangkang shang <535634238@qq.com>
2025-05-16 18:34:12 +0800

    1

--- docs/notes/TypeScript/3.面试题/2.ts
+++ docs/notes/TypeScript/3.面试题/2.ts
@@ -1,6 +1,10 @@
-let alice = { name: "Alice", age: 30, isAdult: true };
-let bob = { name: "Bob", age: 30, isAdult: true } as const;
-alice = bob; // 成功
-alice.name = "new name"; // 修改成功
-
-// bob = alice;
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
+const person: Person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
