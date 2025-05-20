2463c704fd9e147e1671b2acbcb9b1d03d5491b4
kangkang shang <535634238@qq.com>
2025-05-20 23:54:33 +0800

    1

--- docs/.vuepress/theme/components/test.vue
+++ docs/.vuepress/theme/components/test.vue
@@ -2,19 +2,5 @@
   <p>Count: {{ 1 }}</p>
 </template>
 
-<script setup lang="ts">
-class C {
-  _prop = "";
-  get prop() {
-    console.log("读取 prop 属性：", this._prop);
-    return this._prop;
-  }
-  set prop(value) {
-    console.log("设置 prop 属性：", value);
-    this._prop = value;
-  }
-}
-let c = new C();
-c.prop = "123"; // 调用 setter
-console.log('打印 prop：',c.prop); // 调用 getter
+<script setup lang="js">
 </script> 
