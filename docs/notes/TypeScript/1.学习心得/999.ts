c05c53bda7be361ca844c4db63cebf4fe2e24239
kangkang shang <535634238@qq.com>
2025-05-21 23:51:29 +0800

    1

--- docs/notes/TypeScript/1.学习心得/999.ts
+++ docs/notes/TypeScript/1.学习心得/999.ts
@@ -1,16 +1,4 @@
-class C {
-  _prop = "init value";
-  get prop() {
-    console.log("读取 prop 属性：", this._prop);
-    return this._prop;
-  }
-  set prop(value) {
-    console.log("设置 prop 属性：", value);
-    this._prop = value;
-  }
+class Point {
+  public readonly x: number = 1;
+  protected readonly y: number = 1;
 }
-// 实例化
-let c = new C();
-c.prop = "123"; // 调用 setter
-c.prop = 124; // 调用 setter
-console.log("打印 prop：", c.prop); // 调用 getter
