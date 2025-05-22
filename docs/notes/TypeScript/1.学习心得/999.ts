6611847acbf25e50a1ccd7c4a84904d17c7d07e7
kangkang shang <535634238@qq.com>
2025-05-22 17:08:28 +0800

    1

--- docs/notes/TypeScript/1.学习心得/999.ts
+++ docs/notes/TypeScript/1.学习心得/999.ts
@@ -1,12 +1,1 @@
-class A {
-  #name = 1;
-  getName() {
-    return this.#name;
-  }
-}
-
-const a = new A();
-
-console.log(a.getName()); // 1
-console.log(a["#name"]); // undefined
-console.log(a["name"]); // 报错
+class A {}
