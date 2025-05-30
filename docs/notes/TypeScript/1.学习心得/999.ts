f080808fee3f471eef713aaedec1223c517a0d0b
kangkang shang <535634238@qq.com>
2025-05-30 16:54:05 +0800

    1

--- docs/notes/TypeScript/1.学习心得/999.ts
+++ docs/notes/TypeScript/1.学习心得/999.ts
@@ -5,3 +5,25 @@ class Child extends Parent {
   age: number;
 }
 
+class Person {
+  speak = (val: Child) => {
+    return "speak";
+  };
+  hold(val: Child) {
+    return "hold";
+  }
+  breath(val: Parent) {
+    return "breath";
+  }
+}
+class Man extends Person {
+  speak = (val: Parent) => {
+    return "speak";
+  };
+  override hold(val: Parent) {
+    return "hold";
+  }
+  override breath(val: Child) {
+    return "breath";
+  }
+}
