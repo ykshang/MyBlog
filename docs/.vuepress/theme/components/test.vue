f05b6020fd4f691172e07f72c6a5f65c5f6647cc
kangkang shang <535634238@qq.com>
2025-05-30 15:43:09 +0800

    1

--- docs/.vuepress/theme/components/test.vue
+++ docs/.vuepress/theme/components/test.vue
@@ -3,4 +3,26 @@
 </template>
 
 <script setup lang="ts">
+class Parent {
+  name: string;
+}
+class Child extends Parent {
+  age: number;
+}
+class MyClass {
+  myMethod = function (val: Child) {
+    return "";
+  };
+}
+class MyClass1 extends MyClass {
+  myMethod = function (val: Parent) {
+    console.log(this);
+    return "";
+  };
+  
+}
+let aaa = new MyClass1();
+aaa.myMethod(new Child());
+aaa.myMethod({ name: "123" });
+
 </script>
