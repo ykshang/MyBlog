49577891bce8e74e2c9f653c5eb7b99da7f34684
kangkang shang <535634238@qq.com>
2025-06-01 22:54:29 +0800

    1

--- docs/notes/TypeScript/1.学习心得/999.ts
+++ docs/notes/TypeScript/1.学习心得/999.ts
@@ -1,29 +1,9 @@
-class Parent {
+type IUser = {
   name: string;
-}
-class Child extends Parent {
   age: number;
-}
+};
 
-class Person {
-  speak = (val: Child) => {
-    return "speak";
-  };
-  hold(val: Child) {
-    return "hold";
-  }
-  breath(val: Parent) {
-    return "breath";
-  }
-}
-class Man extends Person {
-  speak = (val: Parent) => {
-    return "speak";
-  };
-  override hold(val: Parent) {
-    return "hold";
-  }
-  override breath(val: Child) {
-    return "breath";
-  }
+class aaa implements IUser {
+  name: string;
+  age: number;
 }
