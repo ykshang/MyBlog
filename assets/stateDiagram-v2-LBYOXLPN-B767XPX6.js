8c5cb9dbc9fc91f9400251827539166b0a20dd26
kangkang shang <535634238@qq.com>
2025-07-12 04:38:16 +0000

    Automatic build

---
+++ assets/stateDiagram-v2-LBYOXLPN-B767XPX6.js
@@ -0,0 +1,1 @@
+import{g as e,q as t,B as a,b as s}from"./chunk-Q7NYVSYU-DlYRvJKd.js";import"./chunk-NXEA5HOC-BNDBGXXw.js";import{m as o}from"./mermaid.esm.min-o_SLimXC.js";import"./app-C3ddI6l0.js";var n={parser:a,get db(){return new s(2)},renderer:t,styles:e,init:o(r=>{r.state||(r.state={}),r.state.arrowMarkerAbsolute=r.arrowMarkerAbsolute},"init")};export{n as diagram};
