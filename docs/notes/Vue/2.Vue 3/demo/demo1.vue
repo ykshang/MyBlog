d00bb68cc63d5f70a3bc21c77e974f8f53a6cfb9
kangkang shang <535634238@qq.com>
2025-08-06 16:33:42 +0800

    完成demo1 的包装

---
+++ docs/notes/Vue/2.Vue 3/demo/demo1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="person">
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">年龄+1</button>
+    <button @click="showTel">点我查看联系方式</button>
+  </div>
+</template>
+
+<script setup lang="ts" name="Person">
+import { ref } from "vue";
+// name和age是一个RefImpl的实例对象，简称ref对象，它们的value属性是响应式的。
+let name = ref("张三");
+let age = ref(18);
+// tel就是一个普通的字符串，不是响应式的
+let tel = "13888888888";
+
+function changeName() {
+  // JS中操作ref对象时候需要.value
+  name.value = "李四";
+  console.log(name.value);
+
+  // 注意：name不是响应式的，name.value是响应式的，所以如下代码并不会引起页面的更新。
+  // name = ref('zhang-san')
+}
+function changeAge() {
+  // JS中操作ref对象时候需要.value
+  age.value += 1;
+  console.log(age.value);
+}
+function showTel() {
+  alert(tel);
+}
+</script>
+<style scoped>
+button {
+  margin: 10px 10px 10px 0;
+}
+</style>
