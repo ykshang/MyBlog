97b1518cfc3e50a04d568e46c93a2bd2d13e70ca
kangkang shang <535634238@qq.com>
2025-08-11 23:59:45 +0800

    demo 代码

---
+++ docs/notes/Vue/2.Vue 3/demo/AttrChild.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="child">
+    <div>
+      <div>子组件</div>
+      <div>a：{{ a }}</div>
+      <div>b：{{ b }}</div>
+      <div>{{ $attrs }}</div>
+    </div>
+    <GrandChild :e="e" v-bind="$attrs"></GrandChild>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, useAttrs } from "vue";
+import GrandChild from "./AttrGrandChild.vue";
+defineOptions({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Child",
+});
+defineProps(["a", "b"]);
+const e = ref("e");
+// 在 setup中获取 ＄attrs
+const attr = useAttrs();
+console.log(attr);
+</script>
+<style scoped>
+.child {
+  display: flex;
+  margin: 10px;
+  padding: 20px;
+  border: 2px dashed var(--el-color-primary);
+  flex: 1;
+}
+</style>
---
+++ docs/notes/Vue/2.Vue 3/demo/AttrParent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="parent">
+    <div>
+      <div>父组件</div>
+      <div>a：{{ a }}</div>
+      <div>b：{{ b }}</div>
+      <div>c：{{ c }}</div>
+      <div>d：{{ d }}</div>
+    </div>
+    <Child :a="a" :b="b" :c="c" :d="d"></Child>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import Child from "./AttrChild.vue";
+defineOptions({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Parent",
+});
+
+const a = ref("a");
+const b = ref("b");
+const c = ref("c");
+const d = ref("d");
+</script>
+<style scoped>
+.parent {
+  display: flex;
+  padding: 20px;
+  border: 2px dashed var(--el-color-primary);
+}
+</style>
