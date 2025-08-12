d3146e8acf9b418cd66f22a32f72c1a18f2c68f8
kangkang shang <535634238@qq.com>
2025-08-12 21:29:35 +0800

    调整示例

--- docs/notes/Vue/2.Vue 3/demo/RefChild.vue
+++ docs/notes/Vue/2.Vue 3/demo/RefChild.vue
@@ -2,21 +2,30 @@
   <el-card class="RefChild" header="子组件">
     <div>子：{{ childText }}</div>
     <div>父：{{ parentVal }}</div>
-    <el-button class="btn" type="primary" @click="getParent">获取父数据</el-button>
+    <el-button class="btn" type="primary" @click="getParent"
+      >获取父数据</el-button
+    >
   </el-card>
 </template>
 <script lang="ts" setup>
+// 引入依赖
 import { ElNotification } from "element-plus";
 import { ref, defineExpose, getCurrentInstance } from "vue";
 
+// 组件名称
 defineOptions({ name: "RefChild" });
-const instance = getCurrentInstance();
+
+// 页面两个演示字段
 const childText = ref("孩子");
 const parentVal = ref("");
+
+// 获取来自父组件的内容
+const instance = getCurrentInstance(); // 当前组件实例
 function getParent() {
   parentVal.value = instance?.parent?.exposed?.parentText.value;
   instance?.parent?.exposed?.sayHello("子组件");
 }
+// 对外暴露的 sayHello 方法
 function sayHello(value) {
   ElNotification({
     title: "From 子组件",
--- docs/notes/Vue/2.Vue 3/demo/RefParent.vue
+++ docs/notes/Vue/2.Vue 3/demo/RefParent.vue
@@ -1,26 +1,35 @@
 <template>
   <div class="RefParent">
-    <el-card class="left flex_1" header="父组件" >
+    <el-card class="left flex_1" header="父组件">
       <div>父: {{ parentText }}</div>
       <div>子: {{ childVal }}</div>
-      <el-button type="primary" class="btn" @click="viewChildRef">获取子数据</el-button>
+      <el-button type="primary" class="btn" @click="viewChildRef"
+        >获取子数据</el-button
+      >
     </el-card>
     <RefChild ref="childRef"></RefChild>
   </div>
 </template>
 <script lang="ts" setup>
+// 引入依赖
 import { defineOptions, ref, useTemplateRef } from "vue";
 import RefChild from "./RefChild.vue";
 import { ElNotification } from "element-plus";
+// 组件名称
 defineOptions({ name: "RefParent" });
-
+// 页面字段
 let parentText = ref("父亲");
-let childRef = ref();
 let childVal = ref("");
+
+// 另外一种获取 $refs 的方式
+// let childRef = useTemplateRef('childRef')
+let childRef = ref();
 function viewChildRef() {
   childVal.value = childRef.value.childText;
   childRef.value.sayHello("父组件");
 }
+
+// 对外暴露的 sayHello 方法
 function sayHello(value) {
   ElNotification({
     title: "From 父组件",
