760fd6a2a7239ec0a3299f68a8d565f932d9c19f
kangkang shang <535634238@qq.com>
2025-09-20 22:50:53 +0800

    docs(响应式布局): 更新响应式布局文档并添加示例组件
    重命名并更新响应式布局发展历史文档，添加新的技术应用文档
    新增两个示例组件展示flex和grid布局
    删除旧的历史文档文件

---
+++ docs/响应式布局/demo/2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="wrapper">
+    <div class="demo-panel lightBlue">1</div>
+    <div class="demo-panel lightyellow">2</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  height: 250px;
+  .demo-panel {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    flex: 1;
+  }
+  .lightBlue {
+    background-color: lightblue;
+  }
+  .lightyellow {
+    background-color: lightyellow;
+  }
+}
+</style>
---
+++ docs/响应式布局/demo/3.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div class="item">Item 1</div>
+    <div class="item">Item 2</div>
+    <div class="item">Item 3</div>
+    <div class="item">Item 4</div>
+    <div class="item">Item 5</div>
+    <div class="item">Item 6</div>
+    <div class="item">Item 7</div>
+    <div class="item">Item 8</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-template-rows: repeat(4, 30px);
+  gap: 20px;
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--vp-c-text-1);
+    background-color: var(--el-color-warning);
+  }
+}
+</style>
