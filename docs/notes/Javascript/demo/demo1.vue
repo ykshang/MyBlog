c2ddf740b29362bba66b6dd6912eb82907e4d1ef
kangkang shang <535634238@qq.com>
2025-09-27 22:07:53 +0800

    docs(Javascript): 添加ES6+相关文档和示例
    添加Generator、Symbol、Proxy、Reflect、async/await等ES6+特性的文档和示例代码
    补充Iterator、MessageChannel、mouseEnter/mouseOver等基础知识的文档
    更新Todo列表，移除已完成的题目

---
+++ docs/notes/Javascript/demo/demo1.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-button @click="handleClick">点击查看结果</el-button>
+</template>
+<script lang="ts" setup>
+import { ElMessage } from 'element-plus';
+
+function* helloWorldGenerator() {
+  yield 'hello';
+  yield 'world';
+  return 'ending';
+}
+let hw = helloWorldGenerator();
+function handleClick() {
+  let res = hw.next();
+  let message = JSON.stringify(res);
+  console.log(message);
+  if (res.done && !res.value) {
+    ElMessage.info('Generator 遍历完成')
+  } else {
+    ElMessage.success(message)
+  }
+}
+</script>
